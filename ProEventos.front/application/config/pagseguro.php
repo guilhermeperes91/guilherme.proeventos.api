97b72f7a739fd4bdc11a6faf95455c111b8f4f9d
Guilherme Peres <guilhermefft@gmail.com>
2023-05-20 17:13:20 -0300

    fix: BCK-001 => Adicionado arquivos de database no gitignore e remoção do front.

--- ProEventos.front/application/config/pagseguro.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-
-$config['pagseguroAccount'] = 'pagseguro@esportsbr.com'; // email de acesso ao painel do pagseguro.
-$config['pagseguroToken'] = '85883223F88C4310A1D4CA2D563FF220'; // token do pagseguro
